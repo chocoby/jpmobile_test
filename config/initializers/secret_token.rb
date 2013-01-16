ea844f43bc2b338725f0007410bedd3d946c7bc0
chocoby <chocoby@gmail.com>
2013-01-16 18:11:30 +0900

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JpmobileTest::Application.config.secret_token = '46656eaca6b2b6be7defb688dbfa0bfaa2e560756f147d677140cbba5067f26da0324442ee2e612e2a4f3a499ec095bd064069134ec09089b1ced0101c55cbd2'

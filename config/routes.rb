c4e67ad059a28e90ebec5f1783d0e09215505ffd
chocoby <chocoby@gmail.com>
2013-01-17 20:33:01 +0900

    rails g User / migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 JpmobileTest::Application.routes.draw do
 
+  devise_for :users
+
   get '/sessions' => 'sessions#index'
   get '/sessions/set' => 'sessions#set'
   get '/sessions/delete' => 'sessions#delete'
